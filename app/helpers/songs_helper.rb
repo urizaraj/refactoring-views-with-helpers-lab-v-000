14f0f3453b665c55739efc63c9b1e5a514e3d704
Alexander Urizar <urizaraj@gmail.com>
2018-03-15 21:00:20 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,6 +1,6 @@
 module SongsHelper
   def display_artist(song)
-    if song has artist
+    if song.artist
       link_to song.artist_name, song.artist
     else
       link_to 'Add Artist', edit_song_path(song)
