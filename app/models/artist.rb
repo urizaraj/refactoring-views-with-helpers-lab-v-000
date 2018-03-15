4698ffcfd071dbbbc6edd5da139919af0e5ebe82
Alexander Urizar <urizaraj@gmail.com>
2018-03-15 20:48:13 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,11 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+
+  def artist_name
+    self.artist.name
+  end
+
+  def artist_name=(string)
+    self.artist = Artist.find_or_create_by(name: string)
+  end
 end
